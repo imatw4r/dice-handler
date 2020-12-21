5ccec5e4f9311296e3a13df8b715f4d62e438b38
Bartek Krzys <b.krzys@clearcode.cc>
2020-12-21 18:44:03 +0100

    - Remove unused modules

--- dice_roller/dice_manager/api/routes.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from dice_manager.api.views import DiceManagerViewSet
-
-router = DefaultRouter()
-
-router.register("dicehandle", DiceManagerViewSet)
-
-app_name = "Dice Handler"
-urlpatterns = router.urls
